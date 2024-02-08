6a6398c7b5474a3f39c96a3295c226ddb4808f0e
mikhail <msinitcyn@gmail.com>
2024-02-08 23:14:15 +0200

    BigNum. Fixed. Add/Subtract. Tests

--- src/bignum.cpp
+++ src/bignum.cpp
@@ -1,18 +1,17 @@
 #include <algorithm>
+#include <cmath>
 #include <vector>
 #include <string>
 
+const int BASE = 9;
+
 class BigNum {
-
-    std::vector<int> number;
-    int base;
-
 public:
+    std::vector<int> number;
 
-    BigNum(std::string& s, int base) {
-        this->base = base;
-        for (int i = s.length(); i >= 0; i-=base) {
-            int size_left = std::min(i, base);
+    BigNum(std::string& s) {
+        for (int i = s.length(); i >= 0; i-=BASE) {
+            int size_left = std::min(i, BASE);
             int cur = stoi(s.substr(i-size_left, size_left));
             number.push_back(cur);
         }
@@ -21,17 +20,76 @@ public:
         }
     }
 
-    BigNum(std::string& s): BigNum(s, 9) {
+    BigNum(std::vector<int> v) {
+        if (!v.size()) {
+            number = {0};
+        } else {
+            number = v;
+        }
     }
 
     std::string to_string() {
-        std::string result;
-        for (int i = number.size()-1; i >= 0; --i) {
-            result.append(std::to_string(number[i]));
-        }
-        if (result == "") {
-            result = "0";
+        std::string result = std::to_string(number.back());
+        for (int i = number.size()-2; i >= 0; --i) {
+            std::string next = std::to_string(number[i]);
+            if (next.length() < BASE) {
+                next.append(std::string(BASE-next.length(), '0'));
+            }
+            result.append(next);
         }
         return result;
     }
 };
+
+int compare(BigNum& a, BigNum& b) {
+    if (a.number.size() == b.number.size()) {
+        for (int i = a.number.size()-1; i >= 0; --i) {
+            if (a.number[i] == b.number[i]) {
+                continue;
+            }
+            return a.number[i] > b.number[i] ? 1 : -1;
+        }
+    }
+    return 0;
+}
+
+BigNum add(BigNum& a, BigNum& b) {
+    std::vector<int> result;
+    int carry_in = 0;
+    int base = (int)pow(10, BASE);
+    int i = 0;
+    while (i < a.number.size() || i < b.number.size() || carry_in) {
+        int next = carry_in;
+        next += i < a.number.size() ? a.number[i] : 0;
+        next += i < b.number.size() ? b.number[i] : 0;
+        if (next >= base) {
+            carry_in = 1;
+        }
+        result.push_back(next % base);
+        carry_in = next / base;
+        i++;
+    }
+    return BigNum(result);
+}
+
+BigNum subtract(BigNum& a, BigNum& b) {
+    std::vector<int> result;
+    int base = (int)pow(10, BASE);
+    int borrow_in = 0;
+    for (int i = 0; i < a.number.size() || borrow_in; ++i) {
+        int next =  a.number[i]-borrow_in;
+        next -= b.number.size() <= i ? 0 : b.number[i];
+        if (next < 0) {
+            borrow_in = 1;
+            next += base;
+        } else {
+            borrow_in = 0;
+        }
+        result.push_back(next%base);
+    }
+    int i = 0;
+    while (result.size() > 1 && result.back() == 0) {
+        result.pop_back();
+    }
+    return BigNum(result);
+}
--- test/src/bignum_test.cpp
+++ test/src/bignum_test.cpp
@@ -3,10 +3,76 @@
 #include <gtest/gtest.h>
 #include "../../src/bignum.cpp"
 
-TEST(BigNum_Tests, ToStringTest) {
+class CompareTwoNumbersTest : public testing::TestWithParam<std::tuple<std::string, std::string, int>> {
+};
 
-    std::string s = "123456678901234567890123456789012345678901234567890";
-    BigNum bigNum(s);
+TEST_P(CompareTwoNumbersTest, CompareStrings) {
+    std::string s1 = std::get<0>(GetParam());
+    std::string s2 = std::get<1>(GetParam());
+    int expected = std::get<2>(GetParam());
 
-    EXPECT_EQ(s, bigNum.to_string());
+    BigNum a(s1);
+    BigNum b(s2);
+
+    int result = compare(a, b);
+
+    EXPECT_EQ(result, expected);
 }
+
+INSTANTIATE_TEST_SUITE_P(BigNum_Tests, CompareTwoNumbersTest,
+    testing::Values(
+        std::make_tuple("123", "321", -1),
+        std::make_tuple("1", "1", 0),
+        std::make_tuple("1111", "333", 1)
+    )
+);
+
+class AddTwoNumbersTest : public testing::TestWithParam<std::tuple<std::string, std::string, std::string>> {
+};
+
+TEST_P(AddTwoNumbersTest, AddStrings) {
+    std::string s1 = std::get<0>(GetParam());
+    std::string s2 = std::get<1>(GetParam());
+    std::string expected = std::get<2>(GetParam());
+
+    BigNum a(s1);
+    BigNum b(s2);
+
+    BigNum result = add(a, b);
+
+    EXPECT_EQ(result.to_string(), expected);
+}
+
+INSTANTIATE_TEST_SUITE_P(BigNum_Tests, AddTwoNumbersTest,
+    testing::Values(
+        std::make_tuple("123", "321", "444"),
+        std::make_tuple("99999999999999999999", "1", "100000000000000000000"),
+        std::make_tuple("1", "99999999999999999999", "100000000000000000000"),
+        std::make_tuple("1111", "333", "1444")
+    )
+);
+
+class SubtractTwoNumbersTest : public testing::TestWithParam<std::tuple<std::string, std::string, std::string>> {
+};
+
+TEST_P(SubtractTwoNumbersTest, SubtractStrings) {
+    std::string s1 = std::get<0>(GetParam());
+    std::string s2 = std::get<1>(GetParam());
+    std::string expected = std::get<2>(GetParam());
+
+    BigNum a(s1);
+    BigNum b(s2);
+
+    BigNum result = subtract(a, b);
+
+    EXPECT_EQ(result.to_string(), expected);
+}
+
+INSTANTIATE_TEST_SUITE_P(BigNum_Tests, SubtractTwoNumbersTest,
+    testing::Values(
+        std::make_tuple("444", "321", "123"),
+        std::make_tuple("100000000000000000000", "99999999999999999999", "1"),
+        std::make_tuple("100000000000000000000", "1", "99999999999999999999"),
+        std::make_tuple("1111", "333", "778")
+    )
+);
