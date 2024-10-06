d71114a3266192fb4c2311ed750d0c3f14a7a809
mikhail <msinitcyn@gmail.com>
2024-10-06 23:45:10 +0300

    dsu compression optimization

--- src/dsu.cpp
+++ src/dsu.cpp
@@ -4,36 +4,31 @@
 class DSU {
 
     std::vector<int> parent;
-    std::vector<int> rank;
 
 public:
 
-    DSU(int n) : parent(n), rank(n, 1) {
-        iota(parent.begin(), parent.end(), 0);
+    DSU(int n) : parent(n), rank(n, -1) {
     }
 
     int find(int a) {
-        int set_a = parent[a];
-        if (a != set_a) {
-            parent[a] = find(parent[a]);
+        if (parent[a] < 0) {
+            return a;
         }
+        parent[a] = find(parent[a]);
         return parent[a];
     }
 
     void union_sets(int a, int b) {
         int set_a = find(a);
         int set_b = find(b);
+        if (set_a == set_b) return;
 
-        if (set_a != set_b) {
-            if (rank[set_a] < rank[set_b]) {
-                std::swap(set_a, set_b);
-            }
-
-            if (rank[set_a] == rank[set_b]) {
-                rank[set_a]++;
-            }
-
+        if (parent[set_a] < parent[set_b]) {
+            parent[set_a] += parent[set_b];
             parent[set_b] = set_a;
+        } else {
+            parent[set_b] += parent[set_a];
+            parent[set_a] = set_b;
         }
     }
 };
